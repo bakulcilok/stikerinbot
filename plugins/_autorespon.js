6d004ea0f96e1d4deb34fe9e349c7b75fab071ff
bakulcilok <67635957+bakulcilok@users.noreply.github.com>
2021-12-05 22:54:04 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -30,7 +30,7 @@ handler.all = async function (m, { isBlocked }) {
 ├ 30 Hari / Rp 10,000
 └────
 
-https://github.com/ariffb25/stikerinbot
+https://github.com/bakulcilok/stikerinbot
 `.trim(), '© stikerin', 'Pemilik Bot', ',owner', m)
     }
 
